e7b74215915e63abaf366b16227f8c9ce1b04835
stargazermiao <stargazermiao@gmail.com>
2019-10-04 00:44:40 -0400

    add scc machine in cc

--- pla/cc.rkt
+++ pla/cc.rkt
@@ -55,14 +55,13 @@
 
 ;; fill in the hole of e1 with e2
 (define (fill e1 e2)
-  ;; there should be either a hole in left or right
+  ;; there should be only one hole, although the efficiency is low here
   (match e1
     ['HOLE
      e2]
-    [`([] ,k)
-     `(,e2 ,k)]
-    [`(,k [])
-     `(,k ,e2)]))
+    [`(,l ,r)
+     `(,(fill l e2) ,(fill r e2))]
+    [else e1]))
 
 ;; small step state transition
 ;; greek aplhabet varsigma is usually used as "state"
@@ -99,7 +98,7 @@
     [`((,(? cexp? em) ,(? cexp? en)) ,k)
      `(,em ,(fill k `(HOLE ,en)))]
     [else
-     (displayln "reach normal form")
+     (displayln "reach end")
      #f]
     ))
 
@@ -124,5 +123,92 @@
 
 ;; test output:
 
+;; the init state is ((((λ (x) x) (λ (y) y)) ((λ (z) z) (λ (m) m))) HOLE)
+;; --> (((λ (x) x) (λ (y) y)) (HOLE ((λ (z) z) (λ (m) m))))
+;; --> ((λ (y) y) (HOLE ((λ (z) z) (λ (m) m))))
+;; --> (((λ (y) y) ((λ (z) z) (λ (m) m))) HOLE)
+;; --> (((λ (z) z) (λ (m) m)) ((λ (y) y) HOLE))
+;; --> ((λ (m) m) ((λ (y) y) HOLE))
+;; --> (((λ (y) y) (λ (m) m)) HOLE)
+;; --> ((λ (m) m) HOLE)
+;; reach end
+;; --> #f
+;; '(λ (m) m)
 
+;; in CC Machine, we can easily notice that in case
+;; <V, E[((λX.M) [])]>
+;; this is actually the only possible case for <V, E[(U [])]>, in my toy
+;; language if U is evaled, the only case is  function. Meanwhile consider if
+;; we allow bool and int in lang, these term will get stuck eventually. So
+;; we can already apply substition, there is no need for pull lambda out
+;; then do subst. these two rule can be merged to one.
+;; this modified version of machine is called SCC machine
+
+(define (step-scc ς)
+  (match ς
+    ;; in scc machine we should't exclude value case
+    [`((,(? cexp? em) ,(? cexp? en)) ,k)
+     `(,em ,(fill k `(HOLE ,en)))]
+    ;; if redex is evaled, switch redex from v n to make it form can be evaled
+    ;; v in left
+    ;; hole with nothing left
+    [`(,(? aexp? v) (HOLE ,en))
+     `(,en (,v HOLE))]
+    ;; hole in left
+    [`(,(? aexp? v) ((HOLE ,en) ,k))
+     `(,en ((,v HOLE) ,k))]
+    ;; hole in right
+    [`(,(? aexp? v) (,k (HOLE ,en)))
+     `(,en (,k (,v HOLE)))]
+    ;; v in right, leftside should only be lambda with HOLE
+    ;; hole with nothing left
+    [`(,(? aexp? v) ((λ (,x) ,em) HOLE))
+     `(,(subst em x v) HOLE)]
+    ;; hole in left
+    [`(,(? aexp? v) (((λ (,x) ,em) HOLE) ,k))
+     `(,(subst em x v) (HOLE ,k))]
+    ;; hole in right
+    [`(,(? aexp? v) (,k ((λ (,x) ,em) HOLE)))
+     `(,(subst em x v) (,k HOLE))]
+    [else
+     (displayln "reach end")
+     #f]
+    ))
+
+(define (multistep-scc ς)
+  (let ([next (step-scc ς)])
+    (displayln (format "s--> ~s" next))
+    (if next
+        (multistep-scc next)
+        ς)))
+
+;; specify valid value
+(define (eval-scc ς)
+  (displayln (format "the init state is ~s" ς))
+  (let ([norm (multistep-scc ς)])
+    (match norm
+      [`(,(? aexp? b) HOLE) b]
+      [else (error (format "stuck at ill form ~s" norm))])))
+
+;; inject function should be the same
+
+;; test cc machine
+(eval-scc
+ (inject-cc '(((λ (x) x) (λ (y) y)) ((λ (z) z) (λ (m) m)))))
+
+;; test output
+;; '(λ (m) m)
+;; the init state is ((((λ (x) x) (λ (y) y)) ((λ (z) z) (λ (m) m))) HOLE)
+;; s--> (((λ (x) x) (λ (y) y)) (HOLE ((λ (z) z) (λ (m) m))))
+;; s--> ((λ (x) x) ((HOLE (λ (y) y)) ((λ (z) z) (λ (m) m))))
+;; s--> ((λ (y) y) (((λ (x) x) HOLE) ((λ (z) z) (λ (m) m))))
+;; s--> ((λ (y) y) (HOLE ((λ (z) z) (λ (m) m))))
+;; s--> (((λ (z) z) (λ (m) m)) ((λ (y) y) HOLE))
+;; s--> ((λ (z) z) ((λ (y) y) (HOLE (λ (m) m))))
+;; s--> ((λ (m) m) ((λ (y) y) ((λ (z) z) HOLE)))
+;; s--> ((λ (m) m) ((λ (y) y) HOLE))
+;; s--> ((λ (m) m) HOLE)
+;; reach end
+;; s--> #f
+;; '(λ (m) m)
 
